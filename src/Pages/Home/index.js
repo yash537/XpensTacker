749b5642befe30e32a083615a25faed65c6e7377
yash537 <yashvijaypatel14@gmail.com>
2022-06-19 14:22:01 +0530

    topfold label done

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import Topfold from '../../Components/topfold'
-import './home.css'
+import React from "react";
+import Topfold from "../../Components/topfold";
+import "./home.css";
 const Home = () => {
   return (
-    <div className='home'>
-        <Topfold/>
-        <div>ExpenseList</div>
+    <div className="home">
+      <Topfold />
+      <div>Expenselist of yas patel</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
